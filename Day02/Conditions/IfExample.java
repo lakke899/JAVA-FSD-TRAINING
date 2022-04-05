15a1a092c9cb42bede13990383b9bca4e7b3029f
lakke899 <51609315+lakke899@users.noreply.github.com>
2022-04-05 10:55:35 +0530

    Add files via upload

---
+++ Day02/Conditions/IfExample.java
@@ -0,0 +1,18 @@
+package Conditions;
+
+public class IfExample {
+	public static void main(String[] args)
+	{
+		System.out.println("Start of the program");
+		int a=22,b=20;
+		if(a==b){
+			System.out.println("a is equal to b");
+		}
+		else
+		{
+			System.out.println("a is not equal to b");
+		}
+		System.out.println("end of the program");
+	}
+
+}
